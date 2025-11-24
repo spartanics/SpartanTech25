26ff1c9ecb51266d81638b9b3c44eaa5bebc56bf
Akarshumans <upadhyayakarsh@gmail.com>
2025-11-24 13:48:06 -0600

    Blocker Servo added and Intake power tweaked (#16)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpModeBackwards.java
@@ -0,0 +1,385 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This file contains an example of a Linear "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode is executed.
+ *
+ * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ *
+ * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ *
+ * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+ * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+ * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+ *
+ * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
+ * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
+ * the direction of all 4 motors (see code below).
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="TeleOpNormal", group="Linear OpMode")
+//@Disabled
+public class OmniOpModeBackwards extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    private Servo clawServo = null;
+
+    private int state = 0; // 0: stop_walking 1: start_walking
+    private DcMotor intake = null;
+
+    private final ElapsedTime wiggleTimer = new ElapsedTime();
+    private final ElapsedTime shotTimer = new ElapsedTime();
+    private final ElapsedTime feederTimer = new ElapsedTime();
+    private final ElapsedTime driveTimer = new ElapsedTime();
+    private DcMotorEx launcher;
+    private CRServo rightFeeder;
+    private CRServo leftFeeder;
+    private Servo Blocker;
+
+    private enum LaunchState {
+        IDLE,
+        PREPARE,
+        LAUNCH,
+    }
+
+    private enum WiggleState {
+        WIGGLE_BACK,
+        WIGGLE_FRONT
+    }
+    private WiggleState wiggleState;
+    private enum WiggleControl {
+        WIGGLE_LISTEN,
+        WIGGLING
+    }
+    private WiggleControl wiggleControl;
+    private enum LaunchControl {
+        LISTEN,
+        LAUNCHING,
+    }
+    private LaunchControl launchControl;
+    private LaunchState launchState;
+    final double WIGGLE_TIME = 1.0;
+    final double LAUNCHER_TARGET_VELOCITY = 1125;
+    final double LAUNCHER_MIN_VELOCITY = 900;
+    final double FEED_TIME = 0.20;
+    final double TIME_BETWEEN_SHOTS = 2;
+    public void goForwardFor2Secs() {
+        runtime.reset();
+        while (runtime.seconds() < 2) {
+            // move forward
+            leftFrontDrive.setPower(.2);
+        }
+    }
+    boolean launch(boolean shotRequested){
+        switch (launchState) {
+            case IDLE:
+                if (shotRequested) {
+                    launchState = LaunchState.PREPARE;
+                    shotTimer.reset();
+                }
+                break;
+            case PREPARE:
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY){
+                    launchState = LaunchState.LAUNCH;
+                    leftFeeder.setPower(1);
+                    rightFeeder.setPower(1);
+                    feederTimer.reset();
+                }
+                break;
+            case LAUNCH:
+                if (feederTimer.seconds() > FEED_TIME) {
+                    leftFeeder.setPower(0);
+                    rightFeeder.setPower(0);
+
+                    if(shotTimer.seconds() > TIME_BETWEEN_SHOTS){
+                        launchState = LaunchState.IDLE;
+                        launcher.setVelocity(0);
+                        return true;
+                    }
+                }
+        }
+        return false;
+    }
+
+    boolean wiggleMachine() {
+        switch (wiggleState) {
+            case WIGGLE_BACK:
+                if (wiggleTimer.seconds() > WIGGLE_TIME) {
+                    launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                    wiggleState =  WiggleState.WIGGLE_FRONT;
+                    wiggleTimer.reset();
+                    if (gamepad1.back) {
+                        return true;
+                    }
+                }
+                break;
+            case WIGGLE_FRONT:
+                if (wiggleTimer.seconds() > WIGGLE_TIME) {
+                    launcher.setVelocity(-LAUNCHER_TARGET_VELOCITY);
+
+                    wiggleState = WiggleState.WIGGLE_BACK;
+                    wiggleTimer.reset();
+                    if (gamepad1.back) {
+                        return true;
+                    }
+
+                }
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        rightFeeder = hardwareMap.get(CRServo.class, "right_feeder");
+        leftFeeder = hardwareMap.get(CRServo.class, "left_feeder");
+        Blocker = hardwareMap.get(Servo.class, "blocker");
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        launchState = LaunchState.IDLE;
+        launchControl = LaunchControl.LISTEN;
+        wiggleState = WiggleState.WIGGLE_FRONT;
+        wiggleControl = WiggleControl.WIGGLE_LISTEN;
+
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        wiggleTimer.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+            double intakePower = (gamepad1.left_trigger - gamepad1.right_trigger);
+
+            double feederPower = 0.0;
+
+            // wiggle launch
+            switch (wiggleControl) {
+                case WIGGLE_LISTEN:
+                        if (gamepad1.start) {
+                            wiggleControl = WiggleControl.WIGGLING;
+                        }
+                    break;
+                case WIGGLING:
+                    boolean is_done_wiggling = wiggleMachine();
+                    if (is_done_wiggling) {
+                        launcher.setVelocity(0);
+                        wiggleControl = WiggleControl.WIGGLE_LISTEN;
+                    }
+                    break;
+
+            }
+
+            if (gamepad1.dpad_left)
+                feederPower += 0.4;
+            if (gamepad1.dpad_right)
+                feederPower -= 0.4;
+
+            if (gamepad1.a)
+                Blocker.setPosition(0);
+            if (gamepad1.b)
+                Blocker.setPosition(1);
+
+            if (gamepad1.right_bumper)
+                Blocker.setDirection(Servo.Direction.FORWARD);
+            if (gamepad1.left_bumper)
+                Blocker.setDirection(Servo.Direction.REVERSE); // Move to the middle position (0 to 1 range)
+
+            //Blocker.setPower(0.5); // Spins the continuous servo forward at half power
+            //Blocker.setPower(-0.5); // Spins the continuous servo backward at half power
+            //Blocker.setPower(0); // Stops the continuous servo
+
+            switch (launchControl) {
+                case LISTEN:
+                    leftFeeder.setPower(feederPower);
+                    rightFeeder.setPower(feederPower);
+                    if (gamepad1.y) {
+                        launch(true);
+                        launchControl = LaunchControl.LAUNCHING;
+                    }
+                    break;
+                case LAUNCHING:
+                    if(launch(false)) {
+                        launchControl = LaunchControl.LISTEN;
+                    }
+                    break;
+            }
+
+            if ((axial  != 0) || (lateral != 0)|| (yaw != 0)) {
+                state = 0;
+            }
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+
+            double leftFrontPower  = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower   = axial - lateral + yaw;
+            double rightBackPower  = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+            /*if (gamepad1.start) {
+                runtime.reset();
+                state = 1;
+            }
+
+            if (state == 1) {
+                if (runtime.seconds() < 2) {
+                    leftFrontPower = 0.2;
+                    leftBackPower = 0.2;
+                    rightFrontPower = 0.2;
+                    rightBackPower = 0.2;
+                } else {
+                    leftFrontPower = 0.0;
+                    leftBackPower = 0.0;
+                    rightFrontPower = 0.0;
+                    rightBackPower = 0.0;
+                    state = 0;
+                }
+            }*/
+
+
+            // This is test code:
+            //
+            // Uncomment the following code to test your motor directions.
+            // Each button should make the corresponding motor run FORWARD.
+            //   1) First get all the motors to take to correct positions on the robot
+            //      by adjusting your Robot Configuration if necessary.
+            //   2) Then make sure they run in the correct direction by modifying the
+            //      the setDirection() calls above.
+            // Once the correct motors move in the correct direction re-comment this code.
+
+
+            //leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+            //leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
+            //rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
+           //rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepadhttps://research.google.com/colaboratory/faq.html
+
+
+            // Send calculated power to wheels
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+            intake.setPower(intakePower);
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("intake", intakePower);
+            telemetry.addData("launch_state", launchState);
+            telemetry.addData("launch_control", launchControl);
+            telemetry.addData("launch_velocity","%4.2f", launcher.getVelocity());
+            telemetry.addData("wiggle_state", wiggleState);
+            telemetry.addData("wiggle_control", wiggleControl);
+            telemetry.addData("Status", "Wiggle_Time: " + wiggleTimer.toString());
+            telemetry.update();
+        }
+    }
+
+    private void setPosition(int position) {
+    }
+}
